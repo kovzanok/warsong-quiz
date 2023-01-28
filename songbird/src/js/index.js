e454d428b4c7aebc88c0002116f31d633652a8b8
Alex Kovzan <alexkovzanok@gmail.com>
2023-01-28 18:42:33 +0300

    feat: add rewind ability to player, if the unit is guessed the right name and image are shown

--- songbird/src/js/index.js
+++ songbird/src/js/index.js
@@ -4,9 +4,11 @@ const answers = document.querySelector(".answers");
 const answersArray = [...answers.children];
 const randomNum = Math.floor(Math.random() * 6);
 let isGuessed = false;
-let isPlayingMain=false;
+let isPlayingMain = false;
 const randomAudio = new Audio(unitsData[0][randomNum].audio);
 const questionPlayer = document.querySelector(".question__player");
+const timeBar = document.querySelector(".player__playtime");
+const timeBarPlayed = document.querySelector(".player__playtime_played");
 
 const answersClickHandler = (e) => {
   const answerNumber = answersArray.indexOf(e.target);
@@ -40,7 +42,6 @@ const generateInfoCard = (number) => {
   document.querySelector(".quiz__info").innerHTML = infoCard;
   const infoImage = document.querySelector(".info__image");
   infoImage.style.backgroundImage = `url(${cardData.image})`;
-  
 };
 
 const changeMarkerColor = (targetMarker, answerNumber) => {
@@ -48,36 +49,58 @@ const changeMarkerColor = (targetMarker, answerNumber) => {
     targetMarker.classList.add("answer__marker_green");
     isGuessed = true;
     targetMarker.classList.remove("answer__marker_hover");
+    displayRightAnswer();
   } else {
     targetMarker.classList.add("answer__marker_red");
     targetMarker.classList.remove("answer__marker_hover");
   }
 };
 
+const displayRightAnswer=()=>{
+  const rightName=document.querySelector('.question__name');
+  const rightImage=document.querySelector('.question__image');
+  rightName.textContent=unitsData[0][randomNum].name;
+  rightImage.style.backgroundImage=`url(${unitsData[0][randomNum].image})`;
+}
 const playerClickHandler = (e) => {
-  
   if (e.target.classList.contains("player__control")) {
     const controlButton = e.target;
-    toggleSound(controlButton);
-  }
-  else if(e.target.classList.contains("player__playtime")){
-    
+    togglePlay(controlButton);
+  } else if (e.target.classList.contains("player__playtime")) {
+    const playerTimeline = e.target;
+    rewindSong(e, playerTimeline);
   }
 };
 
-const toggleSound = (controlButton) => {
+const togglePlay = (controlButton) => {
   controlButton.classList.toggle("player__control_play");
   controlButton.classList.toggle("player__control_pause");
-  if (isPlayingMain){
-    isPlayingMain=false;
+  if (isPlayingMain) {
+    isPlayingMain = false;
     randomAudio.pause();
-  }
-  else{
-    isPlayingMain=true;
+  } else {
+    isPlayingMain = true;
     randomAudio.play();
   }
 };
 
-answers.addEventListener("click", answersClickHandler);
+const rewindSong = (e, playerTimeline) => {
+  const partOfDuration = (e.clientX - 487) / playerTimeline.clientWidth;
+  randomAudio.currentTime = partOfDuration * randomAudio.duration;
+  setNewAudioTime();
+};
 
+const setNewAudioTime=()=>{
+  timeBarPlayed.style.width=`${Math.round(randomAudio.currentTime*100/randomAudio.duration)}%`;
+   
+}
+
+const updateTimeBar=()=>{
+  setTimeout(updateTimeBar,100);
+  setNewAudioTime();
+}
+
+answers.addEventListener("click", answersClickHandler);
 questionPlayer.addEventListener("click", playerClickHandler);
+
+updateTimeBar();
