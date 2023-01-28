7460d74cde5ca551ef15b79f68cacac65f398f75
Alex Kovzan <alexkovzanok@gmail.com>
2023-01-28 17:13:14 +0300

    feat: add sound for human race

--- songbird/src/js/data.js
+++ songbird/src/js/data.js
@@ -6,7 +6,7 @@ const unitsData = [
         nameEng: 'Footman',
         description: 'Cильный и выносливый горожанин Азерота, который добровольно пришел в армию Альянса для защиты своих земель.',
         image: './src/img/unit_pic/humans/footman.jpg',
-        audio: ''
+        audio: './src/sound/humans/footman.mp3'
       },
       {
         
@@ -14,7 +14,7 @@ const unitsData = [
         nameEng: 'Arthas',
         description: `Сын короля Теренаса Менетила II, наследный принц Лордерона и рыцарь Серебряной Длани. Позже главный слуга Нер'Зула и его первый Рыцарь Смерти.`,
         image: './src/img/unit_pic/humans/arthas.png',
-        audio: ''
+        audio: './src/sound/humans/arthas.mp3'
       },
       {
         
@@ -22,7 +22,7 @@ const unitsData = [
         nameEng: 'Sorceress',
         description: `Маги-женщины, посылаемые магократами Даларана для слежения за опасными битвами магов. Они используют свои тайные знания для помощи Альянсу, когда человечество находится в опасности.`,
         image: './src/img/unit_pic/humans/sorceress.png',
-        audio: ''
+        audio: './src/sound/humans/sorceress.mp3'
       },
       {
         
@@ -30,7 +30,7 @@ const unitsData = [
         nameEng: 'Peasant',
         description: 'Работников выбирали из трудолюбивых и крепких горожан, которые жили в различных королевствах Лордерона. Добывая золото и дерево для армии, они являются основой Альянса. ',
         image: './src/img/unit_pic/humans/peasant.jpg',
-        audio: ''
+        audio: './src/sound/humans/peasant.mp3'
       },
       {
         
@@ -38,7 +38,7 @@ const unitsData = [
         nameEng: 'Rifleman',
         description: 'Храбрые дворфийские стрелки верно защищают свое горное королевство Каз-Модан, однако они снова были вызваны Альянсом.',
         image: './src/img/unit_pic/humans/rifleman.jpg',
-        audio: ''
+        audio: './src/sound/humans/rifleman.mp3'
       },
       {
         
@@ -46,7 +46,7 @@ const unitsData = [
         nameEng: 'Muradin',
         description: 'Герой Альянса, младший брат Короля Магни Бронзоборода и старший брат известного исследователя Бранна.',
         image: './src/img/unit_pic/humans/muradin.png',
-        audio: ''
+        audio: './src/sound/humans/muradin.mp3'
       }
     ],
     
--- songbird/src/js/index.js
+++ songbird/src/js/index.js
@@ -2,25 +2,22 @@ import unitsData from "./data.js";
 
 const answers = document.querySelector(".answers");
 const answersArray = [...answers.children];
-const randomNum=Math.floor(Math.random() * 6)
-console.log(randomNum);
+const randomNum = Math.floor(Math.random() * 6);
+let isGuessed = false;
+
 const answersCLickHandler = (e) => {
   const answerNumber = answersArray.indexOf(e.target);
+  const targetMarker = e.target.firstElementChild;
+
   generateInfoCard(answerNumber);
-  const targetMarker=e.target.firstElementChild;
-  if (randomNum===answerNumber){
-    targetMarker.classList.add('answer__marker_green');
-    targetMarker.classList.remove('answer__marker_hover');
-  }
-  else{
-    targetMarker.classList.add('answer__marker_red');
-    targetMarker.classList.remove('answer__marker_hover');
+
+  if (!isGuessed) {
+    changeMarkerColor(targetMarker, answerNumber);
   }
 };
 
 const generateInfoCard = (number) => {
   const cardData = unitsData[0][number];
-  
   const infoCard = `<div class="info__head">
     <div class="info__image"></div>
     <div class="info__body">
@@ -39,14 +36,25 @@ const generateInfoCard = (number) => {
       </div>
     </div>
   </div>
-
   <div class="info__description">
     ${cardData.description}
   </div>`;
+
   document.querySelector(".quiz__info").innerHTML = infoCard;
+
   const infoImage = document.querySelector(".info__image");
   infoImage.style.backgroundImage = `url(${cardData.image})`;
+};
 
+const changeMarkerColor = (targetMarker, answerNumber) => {
+  if (randomNum === answerNumber) {
+    targetMarker.classList.add("answer__marker_green");
+    isGuessed = true;
+    targetMarker.classList.remove("answer__marker_hover");
+  } else {
+    targetMarker.classList.add("answer__marker_red");
+    targetMarker.classList.remove("answer__marker_hover");
+  }
 };
 
 answers.addEventListener("click", answersCLickHandler);
