a08eb1b6ed0107e765f58535362e7bc42197f899
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-27 09:55:30 +0300

    refactor: change code for better readability

--- songbird/src/js/index.js
+++ songbird/src/js/index.js
@@ -1,14 +1,9 @@
 import Main from "./Main.js";
-import "../sass/style.scss"
-
+import "../sass/style.scss";
 
 window.onload = () => {
-  const language = localStorage.getItem("language")
-    ? localStorage.getItem("language")
-    : "ru";
+  const language = localStorage.getItem("language") || "ru";
   const main = new Main(language);
   main.createMainPage();
   main.mainPageHandler();
 };
-
-
